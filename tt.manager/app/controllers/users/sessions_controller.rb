afa773b33cfe8c8d75d45107e42ca7d8fe5a5351
Sakamiti-46 <saito.k.13184@gmail.com>
2020-06-23 16:50:07 +0900

    ゲストユーザーログイン機能実装完了

--- tt.manager/app/controllers/users/sessions_controller.rb
+++ tt.manager/app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
+  protect_from_forgery except: :new_guest
   before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,20 +18,17 @@ class Users::SessionsController < Devise::SessionsController
     super
   end
 
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])#attribute])
-  end
-
-  def new_guest
-    user = User.find(1)
-    user.update(email: 'guest@example.com',name: 'ゲストユーザー') do |user|
-      user.password = SecureRandom.urlsafe_base64
-    end
-    sign_in user
-    redirect_to root_path
   end
 
 end
