254c5de9f284ee61b19e7bc88f47d7f30a2b46da
Sakamiti-46 <saito.k.13184@gmail.com>
2020-06-25 20:41:55 +0900

    リモートリポジトリの内容を反映

---
+++ tt.manager/tt.manager/app/controllers/users/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+class Users::SessionsController < Devise::SessionsController
+  protect_from_forgery except: :new_guest
+  before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  def new
+    super
+  end
+
+  # POST /resource/sign_in
+  def create
+    super
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    super
+  end
+
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])#attribute])
+  end
+
+end
