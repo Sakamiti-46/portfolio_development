2d7e42ae550f23e25a2fb0776aabcf58c12b00c2
Sakamiti-46 <saito.k.13184@gmail.com>
2020-06-26 16:08:25 +0900

    production.rb修正

--- tt.manager/config/environments/development.rb
+++ tt.manager/config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = ENV['delivery_method'].to_sym
+  config.action_mailer.delivery_method = ENV['delivery_method'].to_s
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: ENV['smtp_enable_starttls_auto'],
