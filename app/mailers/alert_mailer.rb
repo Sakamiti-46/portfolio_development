753d2e3ad69978209383ee89ff968e94423f4e85
Sakamiti-46 <saito.k.13184@gmail.com>
2020-07-02 10:53:33 +0900

    preview調整中

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,8 +1,7 @@
 class AlertMailer < ApplicationMailer
   def send_alert(user)
     @user = user
-    user.inspect
-    mail to: 'saito.k.13184@gmail.com', subject: '練習記録を登録しましょう！'
+    mail to: @user[:email], subject: '練習記録を登録しましょう！'
     # mail to: "#{@user.email}", subject: "Hello,#{@user.email}"
     # mail to: user.email, subject: '練習記録を登録しましょう！'
   end
--- test/mailers/previews/alert_mailer_preview.rb
+++ test/mailers/previews/alert_mailer_preview.rb
@@ -1,8 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/alert_mailer
 class AlertMailerPreview < ActionMailer::Preview
   def send_alert
-    user = User.all.sample
-    AlertMailer.send_alert
+    @user = User.all
+    AlertMailer.send_alert(@user)
+    # user = User.all.sample
+    # AlertMailer.send_alert
     # AlertMailer.with(user: User.first).send_alert
     # @user = User.find_by(id: user)
     # mail(
