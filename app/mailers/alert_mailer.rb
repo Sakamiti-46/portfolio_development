0b911f2e3b39b34aaba3b9ca1e201492192e494b
Sakamiti-46 <saito.k.13184@gmail.com>
2020-07-03 22:27:27 +0900

    mailer修正

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,8 +1,7 @@
 class AlertMailer < ApplicationMailer
   def send_alert(user)
     @user = user
-    mail to: @user[:email], subject: '練習記録を登録しましょう！'
+    mail to: @user.mail, subject: '練習記録を登録しましょう！'
     # mail to: "#{@user.email}", subject: "Hello,#{@user.email}"
-    # mail to: user.email, subject: '練習記録を登録しましょう！'
   end
 end
--- test/mailers/previews/alert_mailer_preview.rb
+++ test/mailers/previews/alert_mailer_preview.rb
@@ -3,15 +3,6 @@ class AlertMailerPreview < ActionMailer::Preview
   def send_alert
     @user = User.all
     AlertMailer.send_alert(@user)
-    # user = User.all.sample
-    # AlertMailer.send_alert
-    # AlertMailer.with(user: User.first).send_alert
-    # @user = User.find_by(id: user)
-    # mail(
-    #   to: @user.email,
-    #   subject: '練習記録を登録しましょう！'
-    # )
-    # mail to: "#{@user.email}", subject: "Hello,#{@user.email}"
-    # mail to: user.email, subject: '練習記録を登録しましょう！'
+
   end
 end
